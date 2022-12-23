474559420d4752a3eaf44a78172c8a129b9d3665
Nofre Ruiz <nofreruizsalom@gmail.com>
2022-12-23 13:44:34 +0100

    create user controler

--- src/main/java/com/example/esquelet/controller/UserController.java
+++ src/main/java/com/example/esquelet/controller/UserController.java
@@ -1,2 +1,36 @@
-package com.example.esquelet.controller;public class UserController {
+package com.example.esquelet.controller;
+
+import ch.qos.logback.core.model.Model;
+import com.example.esquelet.models.User;
+import com.example.esquelet.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+    @PostMapping("/user") // Change name form name
+    public String userAcces(@ModelAttribute("user") User user , Model model){
+        Optional<User> userOptional = userRepository.searchUserByUsernameEquals(user.getUsername());
+        if(userOptional.isEmpty()) return "index"; // name fail
+        User userDataBase = userOptional.get();
+        if(!userDataBase.getPassword().equals(user.getPassword())) return  "index"; // password fail
+        // user and password done!
+        return "privatePage"; //create page
+    }
+
+    @PostMapping("/addUser")
+    public String addUser(@ModelAttribute("user") User user , Model model){
+
+        
+        return "index";
+    }
+
 }
--- src/main/java/com/example/esquelet/repositories/UserRepository.java
+++ src/main/java/com/example/esquelet/repositories/UserRepository.java
@@ -1,2 +1,22 @@
-package com.example.esquelet.repositories;public interface UserRepository {
+package com.example.esquelet.repositories;
+
+import com.example.esquelet.models.User;
+import org.aspectj.apache.bcel.util.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    /*
+    Hi ha parametres especial que te deixa cercar nomes posant les anotacions
+    L'altre en hi ha empleant @Query
+     */
+    Optional<User> searchUserByUsernameEquals(String username);
+    boolean searchUserByUsernameExists(String username);
+
+
 }
