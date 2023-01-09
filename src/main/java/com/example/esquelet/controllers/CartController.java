b3350bc00301c77facb9752f0f36dceafbc26586
Nofre Ruiz <nofreruizsalom@gmail.com>
2023-01-09 13:10:32 +0100

    cart prove

--- src/main/java/com/example/esquelet/controllers/CartController.java
+++ src/main/java/com/example/esquelet/controllers/CartController.java
@@ -1,2 +1,27 @@
-package com.example.esquelet.controllers;public class CartController {
+package com.example.esquelet.controllers;
+
+import com.example.esquelet.entities.User;
+import com.example.esquelet.models.ArticleSell;
+import com.example.esquelet.models.Cart;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
+
+@Controller
+public class CartController {
+
+    @PostMapping("/{articleSell}")
+    public String addArticle(@ModelAttribute("articleSell") ArticleSell articleSell, Model model, HttpSession session){
+        Cart cart = ( Cart ) session.getAttribute("cart");
+        Cart cartUser = new Cart();
+        if( cart != null ) { cartUser = cart; }
+        cartUser.add( articleSell );
+        session.setAttribute("cart", cartUser );
+        return "index";
+    }
 }
