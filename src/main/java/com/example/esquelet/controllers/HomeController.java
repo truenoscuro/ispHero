013382fe515457c2f69aeaf5e9245de44628302c
Nofre Ruiz <nofreruizsalom@gmail.com>
2023-01-09 12:34:49 +0100

    languageControler done

--- src/main/java/com/example/esquelet/controllers/HomeController.java
+++ src/main/java/com/example/esquelet/controllers/HomeController.java
@@ -1,28 +1,42 @@
 package com.example.esquelet.controllers;
 
+import com.example.esquelet.repositories.LanguageControler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    LanguageControler languageControler;
+
+
     @GetMapping("/")
-    public String index() {
+    public String index( Model model) {
+        model.addAttribute("languages",languageControler.findAll() );
         return "index";
     }
 
+
     @GetMapping("/about")
-    public String about() {
+
+    public String about( Model model ) {
+        model.addAttribute("languages",languageControler.findAll() );
+
         return "about";
     }
 
     @GetMapping("/contact")
-    public String contact() {
+    public String contact( Model model ) {
+        model.addAttribute("languages",languageControler.findAll() );
         return "contact";
     }
 
     @GetMapping("/login")
-    public String login() {
+    public String login( Model model ) {
+        model.addAttribute("languages",languageControler.findAll() );
         return "login";
     }
 
