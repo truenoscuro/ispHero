92d78beeea4278e94bba0ca554c29a19c5a21b9b
Nofre Ruiz <nofreruizsalom@gmail.com>
2023-01-09 18:39:09 +0100

    Cart done

--- src/main/java/com/example/esquelet/controllers/CartController.java
+++ src/main/java/com/example/esquelet/controllers/CartController.java
@@ -5,14 +5,16 @@ import com.example.esquelet.models.Cart;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class CartController {
 
-    @PostMapping("/{articleSell}")
-    public String addArticle(@ModelAttribute("articleSell") ArticleSell articleSell, Model model, HttpSession session){
+    @PostMapping("/")
+    public String addArticle(@ModelAttribute("ArticleSell") ArticleSell articleSell, Model model, HttpSession session){
         Cart cart = ( Cart ) session.getAttribute("cart");
         Cart cartUser = new Cart();
         if( cart != null ) { cartUser = cart; }
@@ -20,4 +22,19 @@ public class CartController {
         session.setAttribute("cart", cartUser );
         return "index";
     }
+
+    @PostMapping("/")
+    public String removeArticle(@ModelAttribute("ArticleSell") ArticleSell articleSell, Model model, HttpSession session){
+        Cart cart = ( Cart ) session.getAttribute("cart");
+        cart.remove(articleSell);
+        return "index";
+    }
+    @GetMapping("/")
+    public String removeAll( Model model, HttpSession session){
+        session.invalidate();
+        return "index";
+    }
+
+
+
 }
