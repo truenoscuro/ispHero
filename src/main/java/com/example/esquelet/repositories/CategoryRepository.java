8427ed08f7c1040283d83332837b678f86096e50
Nofre Ruiz <nofreruizsalom@gmail.com>
2023-01-12 12:29:40 +0100

    list and price in database

--- src/main/java/com/example/esquelet/repositories/CategoryRepository.java
+++ src/main/java/com/example/esquelet/repositories/CategoryRepository.java
@@ -2,9 +2,12 @@ package com.example.esquelet.repositories;
 
 import com.example.esquelet.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository< Category , Long > {
     Optional< Category > searchById( String id );
+
+
 }
