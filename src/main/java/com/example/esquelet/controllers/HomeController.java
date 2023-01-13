d4c83707e62e8a2e8d029a32d04c37c10d16c337
Alexander Witt <92024276+Baumwollgarn@users.noreply.github.com>
2023-01-13 09:43:37 +0100

    Added working email subscription

--- src/main/java/com/example/esquelet/controllers/HomeController.java
+++ src/main/java/com/example/esquelet/controllers/HomeController.java
@@ -1,22 +1,30 @@
 package com.example.esquelet.controllers;
 
+import com.example.esquelet.entities.NewsLetter;
 import com.example.esquelet.repositories.LanguageControler;
+import com.example.esquelet.repositories.NewsLetterRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class HomeController {
 
     @Autowired
     LanguageControler languageControler;
+    @Autowired
+    NewsLetterRepository newsLetterRepository;
 
 
     @GetMapping("/")
-    public String index( Model model) {
+    public String index(Model model) {
         model.addAttribute("languages",languageControler.findAll() );
-
         return "index";
     }
 
