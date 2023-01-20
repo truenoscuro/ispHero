92bee53eb84f622dcafaaaf31289c1030964aa9c
Alexander Witt <92024276+Baumwollgarn@users.noreply.github.com>
2023-01-20 10:54:48 +0100

    hm2

---
+++ src/main/java/com/example/esquelet/services/ArticleService.java
@@ -0,0 +1,47 @@
+package com.example.esquelet.services;
+
+import com.example.esquelet.dtos.ArticleDTO;
+import com.example.esquelet.entities.Product;
+import com.example.esquelet.repositories.CategoryRepository;
+import com.example.esquelet.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+
+@Service
+public class ArticleService {
+    // use for article we sell
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    public List<ArticleDTO> getArticleDTOList(String categoryName) {
+        return categoryRepository.searchByName(categoryName)
+                .get().getProducts()
+                .stream( ).map( this::getArticleDTO )
+                .toList( );
+    }
+    private ArticleDTO getArticleDTO(Product product){
+        return ArticleDTO.createArticleDTO(product.getArticles());
+    }
+
+    public ArticleDTO getArticleDTO(String productName) {
+        return getArticleDTO(productRepository.getProductsByName(productName).get());
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/example/esquelet/services/TranslateService.java
@@ -0,0 +1,20 @@
+package com.example.esquelet.services;
+
+import com.example.esquelet.dtos.TranslateDTO;
+import com.example.esquelet.repositories.LanguageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TranslateService {
+
+    @Autowired
+    LanguageRepository languageRepository;
+
+    public List<TranslateDTO> getAll(){
+        return languageRepository.findAll().stream().map(TranslateDTO::createTranslateDTO).toList();
+    }
+
+}
