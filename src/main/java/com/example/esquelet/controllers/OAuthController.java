07df269c00e5dd44b53db86b13857a7877202586
Alexander Witt <92024276+Baumwollgarn@users.noreply.github.com>
2023-02-03 13:45:12 +0100

    Added dependency

--- src/main/java/com/example/esquelet/controllers/OAuthController.java
+++ src/main/java/com/example/esquelet/controllers/OAuthController.java
@@ -6,8 +6,10 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
 
 @RestController
 public class OAuthController {
@@ -33,10 +37,20 @@ public class OAuthController {
             return new ResponseEntity<>("Unauthorized", null, 401);
         }
         GoogleIdToken.Payload payload = idToken.getPayload();
-        String userId = payload.getSubject();
+        String firstName = (String) payload.get("given_name");
+        String mail = payload.getEmail();
 
         // TODO: From here, you can get the user's email address, name, etc. and CREATE local token
-        String localToken = "LOCALTOKEN "+userId;
+        String localToken = "LOCALTOKEN OF "+ firstName + " WITH EMAIL " + mail;
         return new ResponseEntity<>(localToken, null, 200);
     }
+
+
+    // Test purpose
+   /* @GetMapping("/schedule")
+    public ResponseEntity<List<String>> getSchedule (HttpRequest request) {
+        String token = request.getHeaders().get("Authorization").toString();
+        System.out.println(token);
+        return null;
+    }*/
 }
