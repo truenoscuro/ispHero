ce3b97fe710c007f017e9cbcdce68140b5110de6
Alexander Witt <92024276+Baumwollgarn@users.noreply.github.com>
2023-02-13 14:17:49 +0100

    Bugfixes and email verification

--- src/main/java/com/example/esquelet/controllers/UserController.java
+++ src/main/java/com/example/esquelet/controllers/UserController.java
@@ -1,10 +1,13 @@
 package com.example.esquelet.controllers;
 
 import com.example.esquelet.dtos.UserDTO;
+import com.example.esquelet.entities.User;
+import com.example.esquelet.services.TokenService;
 import com.example.esquelet.services.UserService;
 
 import com.example.esquelet.services.WaitingDomainService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,6 +29,11 @@ public class UserController {
     private UserService userService;
 
 
+    @Autowired
+    TokenService tokenService;
+
+    @Value("${MAIL_API_KEY}")
+    private String MAIL_API_KEY;
     @GetMapping("/register")
     public String register(Model model){
         model.addAttribute("pageTitle","Register");
@@ -36,9 +44,13 @@ public class UserController {
     public String addUser(@ModelAttribute UserDTO user , Model model, @RequestParam(defaultValue = "false") String status) {
         if (Objects.equals(status, "false")) {
             String username = user.getUsername();
-            // Check if any user has the same username
+            // Check if any user has the same username || email
             if (userService.checkUser(username)) {
                 model.addAttribute("error", "Username already exists");
+                return "backendUser/register";
+            }
+            if (userService.checkEmail(user.getEmail())) {
+                model.addAttribute("error", "Email already exists");
                 return "backendUser/register";
             }
             user.setRole("USER");
@@ -103,7 +115,7 @@ public class UserController {
         emailMessage.setHtml(message);
 
         MailerSend mailerSend = new MailerSend();
-        mailerSend.setToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiOGZhMzlmZTdlMmVkZTM0ODJkZDYyMDRkNzQxMmE1MDJlMmM4YWFlNWJkNmZjNmY0ZWQzY2E4NTY3YTFhYTk2ZmEyOGNjY2FiNTNkYjdjOWUiLCJpYXQiOjE2NzUyNDQ0MTguOTIzMSwibmJmIjoxNjc1MjQ0NDE4LjkyMzEwMywiZXhwIjo0ODMwOTE4MDE4LjkxNjc2Nywic3ViIjoiNTYwOTIiLCJzY29wZXMiOlsiZW1haWxfZnVsbCIsImRvbWFpbnNfZnVsbCIsImFjdGl2aXR5X2Z1bGwiLCJhbmFseXRpY3NfZnVsbCIsInRva2Vuc19mdWxsIiwid2ViaG9va3NfZnVsbCIsInRlbXBsYXRlc19mdWxsIiwic3VwcHJlc3Npb25zX2Z1bGwiLCJzbXNfZnVsbCIsImVtYWlsX3ZlcmlmaWNhdGlvbl9mdWxsIiwiaW5ib3VuZHNfZnVsbCIsInJlY2lwaWVudHNfZnVsbCIsInNlbmRlcl9pZGVudGl0eV9mdWxsIl19.fFXonujENY0bK7HoMPrTHWnH-eWBXwuTePS_DFp75ay010aus_zlony_FXhnB01KGx4XSI-GCEG9iJjuzIaPFivOMv62htYniRsZmKFyDFGLSGJmriC4Fzsl4K9MFO9TF_0m69DocCAGlwkE0SD3NWJS0VyQjqJh1oYZLdif4BzMQQYOy41WFMPSqu665IN120C0ZJU4dZdCW78nhbiM3D-lJl2sdDITflqiSyHcMscmg0R-sr8YD_vZsZ0ju81nH24i7Zx2iT7BtDHkfN9aduB7kO0YlTofT_zFGY6FntHs1Ub1axy6Tlg0Bg_UpoPL3Baf8qIuMlsXnvmPleRP3GKPNUJdRd-SkT4JD8voEtEO6eoNf6Xbz5R6_aSlkIFdNfXYDWfVRR-KzOtuAtQAZZ7JxzC_a1wSAFm9cYndl1C83ZkSrONio8m-uab3cgqmpZ-51ZtxgEfxoJyAMZeoxkNQPjONvGNXgDsmR2ktGeDrTzXgwUpUvEn9tHQkWuwUTzgQRBKGt1rkDiNhzQ9l0D4f0ZVROY6lCEP4yMQq6KWKBf32H3VovE9O3TFBJY9OYx17tnUykWCw81NYx0Z8sx68gZhE_GzBjZKi42aOifcy3k11JhKIF_svcl4dxQDvaUcqUblwlWpcZXSYM-hnyeSlShPqZUQS2W11_zDbw4I");
+        mailerSend.setToken(MAIL_API_KEY);
 
         try {
             MailerSendResponse response = mailerSend.emails().send(emailMessage);
@@ -131,4 +143,26 @@ public class UserController {
         return "backendUser/messagesent";
     }
 
+    @GetMapping("/validate/{token}")
+    public String validate(@PathVariable String token, Model model) {
+        if (tokenService.validateToken(token) == 0) {
+            tokenService.getClaims(token);
+            String email = tokenService.getClaims(token).get("email").toString();
+            User user = userService.getUserByEmail(email);
+            user.setVerified(true);
+            userService.updateUser(user);
+            model.addAttribute("status", "verified");
+            return "backendUser/register";
+        } else if (tokenService.validateToken(token) == 1) {
+            System.out.println("Token expired");
+            model.addAttribute("status", "expired");
+            model.addAttribute("error", "Token expired. Please contact the administrator or register again.");
+            return "backendUser/register";
+        } else {
+            System.out.println("Token invalid");
+            model.addAttribute("status", "invalid");
+            model.addAttribute("error", "Token invalid. Please contact the administrator or register again.");
+            return "backendUser/register";
+        }
+    }
 }
