2df1762f50ca4e8f8e742a07d551c3bcaabd13e7
Nofre Ruiz <nofreruizsalom@gmail.com>
2023-02-17 11:56:24 +0100

    Languages update , charge translates with ddbb

--- src/main/java/com/example/esquelet/config/LanguageConfig.java
+++ src/main/java/com/example/esquelet/config/LanguageConfig.java
@@ -1,12 +1,11 @@
 package com.example.esquelet.config;
-
-import jakarta.servlet.http.HttpSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
 
 import java.util.Locale;
 
